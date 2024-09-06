90ba03081a45841887757c65b6bdc52f4b2969e9
Koray Topuz <krytopuz@gmail.com>
2024-09-07 02:34:07 +0300

    .

--- Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -11,19 +11,19 @@ namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
 {
     public class UpdateAboutCommandHandler
     {
-        private readonly IRepository<About> _reporsitory;
+        private readonly IRepository<About> _repository;
 
-        public UpdateAboutCommandHandler(IRepository<About> reporsitory)
+        public UpdateAboutCommandHandler(IRepository<About> repository)
         {
-            _reporsitory = reporsitory;
+            _repository = repository;
         }
         public async Task Handle(UpdateAboutCommand command)
         {
-            var values=await _reporsitory.GetByIdAsync(command.AboutID);
+            var values=await _repository.GetByIdAsync(command.AboutID);
             values.Description = command.Description;
             values.Title = command.Title;
             values.ImageUrl = command.ImageUrl;
-            await _reporsitory.UpdateAsync(values);
+            await _repository.UpdateAsync(values);
         }
     }
 }
