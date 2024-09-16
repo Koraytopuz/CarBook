884c22e0bb48c4b30c55e7d3617dbcb233715d10
Koray Topuz <krytopuz@gmail.com>
2024-09-16 23:00:09 +0300

    ..

--- Core/CarBook.Application/Features/Mediator/Commands/LocationCommands/CreateLocationCommand.cs
+++ Core/CarBook.Application/Features/Mediator/Commands/LocationCommands/CreateLocationCommand.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarBook.Domain.Entites
+namespace CarBook.Application.Features.Mediator.Commands.LocationCommands
 {
-    public class Location
+    public class CreateLocationCommand :IRequest
     {
-        public int LocationID { get; set; }
         public string Name { get; set; }
     }
 }
