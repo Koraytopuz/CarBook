dfa33c2a17cba7f10040cb9de9a9cf0fe49048fe
Koray Topuz <krytopuz@gmail.com>
2024-09-22 03:09:25 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -4,8 +4,22 @@ namespace CarBook.WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values =JsonConvert.DeserializeObject<List<string, object>>(jsonData);
+            }
             return View();
         }
     }
