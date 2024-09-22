3ddf6f2aebe20d4004c57f498813f3c7d3057ffa
Koray Topuz <krytopuz@gmail.com>
2024-09-22 03:36:08 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.AboutViewComponents
 {
@@ -18,7 +20,8 @@ namespace CarBook.WebUI.ViewComponents.AboutViewComponents
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData= await responseMessage.Content.ReadAsStringAsync();
-                var values =JsonConvert.DeserializeObject<List<string, object>>(jsonData);
+                var values =JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
