766b8e5067a39be657d6d0878459191d15040e7e
Koray Topuz <krytopuz@gmail.com>
2024-11-05 15:18:31 +0300

    .

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -4,8 +4,22 @@ namespace CarBook.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Cars/GetLast5CarsWithBrandQueryHandler\r\n");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+
+            }
             return View();
         }
     }
