3f4218ab3e5218511be98cdb0abb4721c730c4ae
Koray Topuz <krytopuz@gmail.com>
2024-11-06 18:18:52 +0300

    .

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -14,10 +16,12 @@ namespace CarBook.WebUI.Controllers
         public async  Task<IActionResult> Index()
         {
             var client=_httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7169/api/Cars/GetLast5CarsWithBrandQueryHandler\r\n");
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Cars/GetCarWithBrand");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultCarWithBrandDtos>>(jsonData); 
+                return View(values);
 
             }
             return View();
