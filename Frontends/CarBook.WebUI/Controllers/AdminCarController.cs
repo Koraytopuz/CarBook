56ac9c018c3f586a909d016d97d066338a20adf2
Koray Topuz <krytopuz@gmail.com>
2024-11-08 00:38:41 +0300

    .

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using CarBook.Dto.CarDtos;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
@@ -27,8 +29,19 @@ namespace CarBook.WebUI.Controllers
             return View();
         }
             [HttpGet]
-            public IActionResult CreateCar()
+            public async  Task<IActionResult> CreateCar()
         {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues=(from x in values
+                                              select new  SelectListItem
+                                              {
+                                                  Text=x.name,
+                                                  Value=x.brandId.ToString()
+                                              }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
     }
