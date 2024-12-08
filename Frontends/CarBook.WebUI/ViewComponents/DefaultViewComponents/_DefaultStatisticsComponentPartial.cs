2db7d518dfdbe4db463d264f61ecd316b4952911
Koray Topuz <krytopuz@gmail.com>
2024-12-09 01:18:21 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
 	public class _DefaultStatisticsComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
 		{
-			return View();
+            var client = _httpClientFactory.CreateClient();
+
+            #region Car
+            var responseMessage = await client.GetAsync("https://localhost:7169/api/Statistics/GetCarCount");
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.carCount = values.carCount;
+            }
+            #endregion
+
+            #region Location
+            var responseMessage2 = await client.GetAsync("https://localhost:7169/api/Statistics/GetLocationCount");
+            {
+
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData2);
+                ViewBag.locationCount = values2.locationCount;
+            }
+            #endregion
+
+            #region Brand
+            var responseMessage3 = await client.GetAsync("https://localhost:7169/api/Statistics/GetBrandCount");
+            {
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData3);
+                ViewBag.brandCount = values3.brandCount;
+            }
+            #endregion
+
+            #region carCountByFuelElectric
+            var responseMessage4 = await client.GetAsync("https://localhost:7169/api/Statistics/GetCarCountByFuelElectric");
+            {
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData4);
+                ViewBag.carCountByFuelElectric = values4.carCountByFuelElectric;
+            }
+            #endregion
+
+            return View();
+
+
 		}
 	}
 }
